03c02130485d415a71325dec5ef0be0311d437fb
kimzing <kimzing@163.com>
2020-12-25 15:31:50 +0800

    回退mybatis功能

--- kimzing-springboot-starter/src/main/java/com/kimzing/autoconfigure/MyBatisPlusConfiguration.java
+++ kimzing-springboot-starter/src/main/java/com/kimzing/autoconfigure/MyBatisPlusConfiguration.java
@@ -1,12 +1,16 @@
 package com.kimzing.autoconfigure;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import com.kimzing.autoconfigure.properties.MyBatisPlusProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * MyBatisPlus配置.
@@ -15,22 +19,36 @@ import org.springframework.context.annotation.Configuration;
  * @since 2020/7/5 16:40
  */
 @Configuration
+@EnableConfigurationProperties({MyBatisPlusProperties.class})
+@ConditionalOnClass({PaginationInterceptor.class, JsqlParserCountOptimize.class})
 public class MyBatisPlusConfiguration {
 
     /**
-     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * 分页配置
+     * @return
      */
     @Bean
     @ConditionalOnProperty(prefix = "kimzing.mybatis-plus.page", name = "enabled", havingValue = "true", matchIfMissing = true)
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
+    @ConditionalOnMissingBean(PaginationInterceptor.class)
+    public PaginationInterceptor paginationInterceptor(MyBatisPlusProperties myBatisPlusProperties) {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(myBatisPlusProperties.getPage().getOptimizeJoin()));
+        return paginationInterceptor;
     }
 
+    /**
+     * SQL执行效率插件,仅dev/test/sit环境开启
+     */
     @Bean
-    public ConfigurationCustomizer configurationCustomizer() {
-        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    @Profile({"dev","test","sit","perf","uat"})
+    @ConditionalOnProperty(prefix = "kimzing.mybatis-plus.performance", name = "enabled", havingValue = "true", matchIfMissing = true)
+    @ConditionalOnMissingBean(PerformanceInterceptor.class)
+    public PerformanceInterceptor performanceInterceptor(MyBatisPlusProperties myBatisPlusProperties) {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setFormat(myBatisPlusProperties.getPerformance().getFormate());
+        performanceInterceptor.setMaxTime(myBatisPlusProperties.getPerformance().getMaxTime());
+        return performanceInterceptor;
     }
 
 }
